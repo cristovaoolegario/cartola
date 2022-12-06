910995a26092ad734611a03ad514a0c8f33a1354
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-06 20:23:16 -0300

    chore(consolidation-ms): add team entity struct and tests

---
+++ consolidation-service/internal/domain/entity/team_test.go
@@ -0,0 +1,23 @@
+package entity
+
+import "testing"
+
+func TestTeam(t *testing.T){
+	team := NewTeam("123", "Test team")
+	player := NewPlayer("123", "Test player name", 10.0)
+	team.AddPlayer(player)
+
+	t.Run("Should add player to team When add player is called", func(t *testing.T){
+		if len(team.Players) < 1 {
+			t.Errorf("Should've added a player to the team, any player was found in the team.")
+		}
+	})
+
+	team.RemovePlayer(player)
+
+	t.Run("Should remove player from team When remove player is called", func(t *testing.T){
+		if len(team.Players) > 0 {
+			t.Errorf("Should'nt have any player in the team, found %d", len(team.Players))
+		}
+	})
+}
