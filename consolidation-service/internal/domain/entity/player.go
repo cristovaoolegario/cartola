60c104cd6fff752abfb39a5691865c05cccfc4d9
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-06 20:44:28 -0300

    refactor(consolidation-ms): format go files

--- consolidation-service/internal/domain/entity/player.go
+++ consolidation-service/internal/domain/entity/player.go
@@ -1,15 +1,15 @@
 package entity
 
 type Player struct {
-	ID string
-	Name string
+	ID    string
+	Name  string
 	Price float64
 }
 
-func NewPlayer(id, name string, price float64) *Player{
+func NewPlayer(id, name string, price float64) *Player {
 	return &Player{
-		ID: id,
-		Name: name,
+		ID:    id,
+		Name:  name,
 		Price: price,
 	}
 }
--- consolidation-service/internal/domain/entity/team.go
+++ consolidation-service/internal/domain/entity/team.go
@@ -1,27 +1,27 @@
 package entity
 
-type Team struct{
-	ID string
-	Name string
+type Team struct {
+	ID      string
+	Name    string
 	Players []*Player
 }
 
-func NewTeam(id, name string) *Team{
+func NewTeam(id, name string) *Team {
 	return &Team{
-		ID: id,
+		ID:   id,
 		Name: name,
 	}
 }
 
-func (t *Team) AddPlayer(player *Player){
+func (t *Team) AddPlayer(player *Player) {
 	t.Players = append(t.Players, player)
 }
 
-func (t *Team) RemovePlayer(player *Player){
+func (t *Team) RemovePlayer(player *Player) {
 	for i, p := range t.Players {
-		if p.ID == player.ID{
-			t.Players = append(t.Players[:i],t.Players[i+1:]...)
+		if p.ID == player.ID {
+			t.Players = append(t.Players[:i], t.Players[i+1:]...)
 			return
-		}		
+		}
 	}
 }
