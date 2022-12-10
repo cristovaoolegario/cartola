b889055b56648571525e7e301693e44ec906df8b
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-09 22:45:07 -0300

    chore(django-panel): Add Player and Team models

--- django-panel/app/admin.py
+++ django-panel/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from app.models import Player, Team
+
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Team)
--- django-panel/app/models.py
+++ django-panel/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+  name = models.CharField(max_length=50)
+  initial_price = models.FloatField()
+
+  def __str__(self):
+    return self.name
+
+class Team(models.Model):
+  name = models.CharField(max_length=50)
+  
+  def __str__(self):
+        return self.name
