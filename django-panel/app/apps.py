0863184eacc650cc3c74fbccce47e1cc47d3f001
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-11 20:43:15 -0300

    chore(django-panel): Add receivers for models

--- django-panel/app/apps.py
+++ django-panel/app/apps.py
@@ -5,3 +5,5 @@ class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
 
+    def ready(self) -> None:
+        import app.receivers
---
+++ django-panel/app/receivers.py
@@ -0,0 +1,37 @@
+
+from .models import Action, Match, Player, Team
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=Player)
+def publish_player_created(sender, instance: Player, created: bool, **kwargs):
+  if created:
+    print('Player created')
+
+@receiver(post_save, sender=Team)
+def publish_team_created(sender, instance: Team, created: bool, **kwargs):
+  if created:
+    print('Team created')
+
+@receiver(pre_save, sender=Match)
+def get_old_match(sender, instance: Match, **kwargs):
+    try:
+        instance._pre_save_instance = Match.objects.get(pk=instance.pk)
+    except Match.DoesNotExist:
+        instance._pre_save_instance = None
+    # publish('match_updated', instance)
+
+@receiver(post_save, sender=Match)
+def publish_match_created(sender, instance: Match, created: bool, **kwargs):
+  if created:
+    print('Match created')
+
+@receiver(post_save, sender=Match)
+def publish_new_match_result(sender, instance: Match, created: bool, **kwargs):
+  if not created:
+    print('Match result published')
+
+@receiver(post_save, sender=Action)
+def publish_action_added(sender, instance: Action, created: bool, **kwargs):
+  if created:
+    print('Action result published')
