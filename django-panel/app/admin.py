2dc00545fe843eadf6c792cdf0e9a32e468b8f84
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-10 23:54:03 -0300

    chore(django-panel): Add Match model

--- django-panel/app/admin.py
+++ django-panel/app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from app.models import Player, Team, MyTeam
+from app.models import Match, Player, Team, MyTeam
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(MyTeam)
+
+admin.site.register(Match)
--- django-panel/app/models.py
+++ django-panel/app/models.py
@@ -19,3 +19,15 @@ class MyTeam(models.Model):
 
   def __str__(self):
         return [player.name for player in self.players.all()].__str__()
+
+class Match(models.Model):
+    match_date = models.DateTimeField()
+    team_a = models.ForeignKey(
+        Team, on_delete=models.PROTECT, related_name='team_a_matches')
+    team_a_goal = models.IntegerField(default=0)
+    team_b = models.ForeignKey(
+        Team, on_delete=models.PROTECT, related_name='team_b_matches')
+    team_b_goal = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.team_a.name} x {self.team_b.name}"
