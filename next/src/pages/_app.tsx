c9aedda1c301538839e72b71935bf8cd8e0738f6
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-13 18:37:16 -0300

    chore(next-app): Update app and _document

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+//import theme from "../util/theme";
+import createEmotionCache from "../util/createEmotionCache";
+//import { Navbar } from "../components/Navbar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <CssBaseline />
+      {/*<Navbar/>*/}
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
