56651c12d6e81b9d639d5dbbe78fa9033680cf40
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-12-14 18:17:35 -0300

    chore(next-app): Add Page component

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@mui/material";
 import Head from "next/head";
 import Image from "next/image";
+import { Page } from "../components/Page";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <Button variant="contained">Test</Button>;
+  return (
+    <Page>
+      <Button variant="contained">Test</Button>
+    </Page>
+  );
 }
